e324e35f20e737a909bbc226b07a047bd7c086c6
McGiverSystem <McGiverSystem@MCGIVERSYSTEM>
2021-10-08 17:10:45 -0400

    Agregar archivos de proyecto.

---
+++ Arreglos/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace Arreglos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            float[] promedio = new float[] { 5, 10, 15, 20, 25, 30 };
+            float media = (promedio.Sum()/6);
+
+            Console.WriteLine("{0}", media);
+            
+
+        }
+
+
+    }
+    
+        
+ 
+
+
+       
+             
+        
+}
